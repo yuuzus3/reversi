830bd4c787a66d9d8a78823af68157bdd2cc2ce6
Yu Morimoto <ytmorimo@uci.edu>
2020-05-13 21:36:29 -0700

    added socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,12 @@ if('undefined' == typeof username || !username){
 }
 
 $('#messages').append('<h4>'+username+'</h4>');
+
+/* Connect to the socket server */
+
+var socket = io.connect();
+
+socket.on('log',function(array) {
+  console.log.apply(console,array)
+}
+);
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/*****************************************************/
+/* Set up the static file server */
+
 /* Include the static file webserver library */
 var static = require('node-static');
 
@@ -29,3 +32,24 @@ var app = http.createServer(
 ).listen(port);
 
 console.log('The Server is running');
+
+/*****************************************************/
+/* Set up the web socket server */
+
+var io = require('socket.io').listen(app);
+
+io.sockets.on('connection',function (socket){
+  function log(){
+    var array = ['*** Server Log Message: '];
+    for(var i = 0; i < arguments.length; i++){
+      array.push(arguments[i]);
+      console.log(arguments[i]);
+    }
+    socket.emit('log',array);
+    socket.broadcast.emit('log',array);
+  }
+    log('A web site connected to the server');
+  socket.on('disconnect',function(socket){
+    log('A web site disconnected from the server');
+  });
+});
